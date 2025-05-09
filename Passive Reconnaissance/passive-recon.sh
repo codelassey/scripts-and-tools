f72af7f97981e6be90f5cba76c589d0d572977a5
Prince Kwesi Lassey <lasseyprincek@gmail.com>
2025-05-09 13:10:54 +0000

    Add files via upload

--- Passive Reconnaissance/passive-recon.sh
+++ Passive Reconnaissance/passive-recon.sh
@@ -1,32 +1,34 @@
 !#/bin/bash
+echo "========================================================================="
 figlet "Domain Passive Recon"
+echo "========================================================================="
 
 read -p "Enter domain/ip address to scan: " target
 
-mkdir -p "${target} Passive Recon Results"
+mkdir -p "$(echo "$target" | tr '/' '_') Passive Recon Results"
 
 # WHOIS lookup
 echo ""
 echo "WHOIS Lookup is running :)"
-whois "${target}" > "${target} Passive Recon Results/whois.txt"
+whois "${target}" > "$(echo "$target" | tr '/' '_') Passive Recon Results/whois.txt"
 echo "WHOIS Lookup Complete."
 
 # Nslookup
 echo ""
 echo "Nslookup is running :) "
-nslookup "${target}" > "${target} Passive Recon Results/nslookup.txt"
+nslookup "${target}" > "$(echo "$target" | tr '/' '_') Passive Recon Results/nslookup.txt"
 echo "Nslookup Complete"
 
 # HOST lookup
 echo ""
 echo "HOST lookup is running :)"
-host "$target" > "${target} Passive Recon Results/host.txt"
+host "$target" > "$(echo "$target" | tr '/' '_') Passive Recon Results/host.txt"
 echo "HOST Lookup Complete"
 
 # DIG lookup
 echo ""
 echo "DIG lookup is running :)"
-dig "$target" any > "${target} Passive Recon Results/dig.txt"
+dig "$target" any > "$(echo "$target" | tr '/' '_') Passive Recon Results/dig.txt"
 echo "DIG Lookup Complete."
 
 echo ""
@@ -36,11 +38,11 @@ TheHarvester is running :)
 Searching all public data...
 Might take just some minutes..
 EOF
-theHarvester -d "$target" -b all 2>/dev/null > "${target} Passive Recon Results/Theharvester.txt"
+theHarvester -d "$target" -b all 2>/dev/null > "$(echo "$target" | tr '/' '_') Passive Recon Results/Theharvester.txt"
 echo "TheHarvester Scan Complete."
 
 echo ""
 
 echo "================================================================================="
-echo "Recon Complete! Results are saved in '${target} Passive Recon Results' directory"
+echo "Recon Complete! Results are saved in '$(echo "$target" | tr '/' '_') Passive Recon Results' directory"
 echo "================================================================================="
